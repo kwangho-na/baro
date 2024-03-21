6818c01b31baf6e6680181a8be14b993f2d085d5
kwangho-na <36679132+kwangho-na@users.noreply.github.com>
2024-03-21 19:21:01 +0900

    Update str.js
    aa

--- data/libs/common/str.js
+++ data/libs/common/str.js
@@ -1,5 +1,5 @@
 <func>
-	@str.fmt(&s) {
+	ff(&s) {
 		fn=Cf.funcNode('parent')
 		a=args(1)
 		ss=''
@@ -13,4 +13,42 @@
 		}
 		return ss
 	}  
+</func>
+
+<func>
+showInputs() {
+	grid=this
+	cur=grid.lastNode(true)
+	not(cur) return;
+	fields=grid.fields()
+	hh=grid.headerHeight()
+	base='inline'
+	this.makeInput(base,fields.size())
+	while(n=0, n<fields.size(), n++) {
+		rc=rc(grid.nodeRect(cur,n)).incrXW(1,-1)
+		input=object("input.${base}:input_$n")
+		input.parentWidget(grid)
+		input.styleSheet('border: 1px solid #449')
+		input.move(rc.incrY(hh,true))
+		input.show()
+	}
+	input=object("input.${base}:input_0").focus()
+}
+makeInput(base,num) {
+	if(Cf.getObject("input","${base}:input_0")) return;
+	evt=#[
+		onKeyDown(k,s){ this.parentWidget().keydown(k,s) }
+	]
+	ss=''
+	while(n=0,n<num,n++) ss.add(ff('<input id="input_{n}">{evt}</input>'))
+	Cf.sourceApply(ff('<widgets base="{base}">{ss}</widgets>'))
+}
+hideInputs() {
+	grid=this
+	fields=grid.fields()
+	while(n=0, n<fields.size(), n++) {
+		input=object("input.aa:input_$n")
+		input.hide()
+	}	
+}	
 </func>
