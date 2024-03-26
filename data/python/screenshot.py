9d22fe9011df5d826f8b6fc806afae53a12f9658
kwangho-na <36679132+kwangho-na@users.noreply.github.com>
2024-03-26 16:48:37 +0900

    Update screenshot.py
    test

--- data/python/screenshot.py
+++ data/python/screenshot.py
@@ -1,3 +1,25 @@
+import qrcode
+import cv2
+qr = qrcode.make("Manan Patel")
+qr.save("Qrcode.png")
+d= cv2.QRCodeDetector()
+val , points, straight_qrcode = d.detectAndDecode(cv2.imread("Qrcode.png"))
+print(val)
+
+import cv2
+img = cv2.imread("1.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(gray)
+blur = cv2.GaussianBlur(invert,(21,21),0)
+invertBlure = cv2.bitwise_not(blur)
+sketch = cv2.divide(gray,invertBlure,scale=256.0)
+# cv2.imshow("gray",gray)
+# cv2.imshow("invert",invert)
+# cv2.imshow("invertBlure",invertBlure)
+cv2.imshow("Sketch",sketch)
+cv2.waitKey(0)
+
+
 import sys
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import QPixmap, QScreen
