14a671d64253cc9ef4d5106abad562b97df0945c
kwangho-na <36679132+kwangho-na@users.noreply.github.com>
2024-05-02 20:55:00 +0900

    Update editor.js

--- data/pages/editor.js
+++ data/pages/editor.js
@@ -3,5 +3,27 @@ lineCopy(e) {
   s=e.text(sp,ep)
   e.move(ep)
   e.insert("\r\n$s", true)
-  print("s==$s", sp,ep)
+}
+
+insertIndent(e, &str, indent, pos ) {
+  if( pos ) this.move(pos);
+  not( indent ) {
+    line=this.sp('lineStart').spText();
+    indent=indentText(line);
+  }
+  this.insert( rst, true);
+  return true;
+}
+
+str.indenText(&s, indent) {
+  ss='';
+  not(indent) indent='';
+  not(lineCheck(s) ) s.findPos("\n")
+  while( s.valid(), num ) {
+    left=s.findPos("\n").trim();
+    not(left) continue;
+    if(num) ss.add("\n", indent);
+    ss.add(left);
+  }
+  return ss
 }
