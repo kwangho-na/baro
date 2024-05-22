ef2c664a51f697ca66de4ce90e78099972e7752a
kwangho-na <36679132+kwangho-na@users.noreply.github.com>
2024-05-22 19:15:36 +0900

    Create widget.js
    first

---
+++ temp/widget.js
@@ -0,0 +1,65 @@
+class widget {	 
+	addChild() {
+		print("widget add child")
+	}
+	base() {
+		base=this.var(baseCode)
+		not(base ) base
+		return when(base.find(':'), left(base,':'), base);
+	}
+	conf(name) {
+		base=this.base()
+		return conf("${base}.${name}")
+	}
+}
+
+ 
+class conf {
+editorSrc: 
+	<page id="${pageId}">
+		<editor id="editor">
+	</page>
+}
+
+class layout {
+	<page id="main" margin="0" onInit() {
+		this.put(sep, titleBar)
+		class(this,'widget')
+		class(this,'funcEditorMain')
+		class(titleBar, 'funcEditorTitleBar')
+		
+	}>
+		<context id="titleBar" height="40">
+		<splitter id="sep">
+	</page>
+	
+	<page id="leftPanel" onInit() {
+		this.put(leftMenu, menuBar)
+		class(leftMenu, 'test_leftMenu')
+		class(menuBar, 'test_leftMenuBar')
+	}>
+		<tree id="leftMenu">
+		<context id="menuBar">
+	</page>
+	<page id="contentPanel">
+		<div id="content">
+	</page>
+}
+
+class funcEditorMain {
+	
+	addEditor(pageId) {
+		base=this.base()
+		src=fmt(this.conf("editorSrc"))
+		Cf.sourceApply(#[
+			<widgets base="${base}">${src}</widgets>
+		])
+	}
+	
+}
+class funcEditorTitleBar {
+	onDraw(dc, rc) {
+		dc.fill('#eee')
+		dc.rectLine(rc.incrH(-1), 4, '#ccc', 2)
+	}
+}
