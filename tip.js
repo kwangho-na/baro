2858cd3a0914d9634fe157cec42d590549528b25
kwangho-na <36679132+kwangho-na@users.noreply.github.com>
2024-05-31 04:20:48 +0900

    Update tip.js

--- tip.js
+++ tip.js
@@ -6,3 +6,29 @@ hello world
 https://hoil2.tistory.com/32
 
 https://devsnap.me/three-js-examples
+
+<widgets base="test">
+	<page id="p1">
+		<video id="v">
+		<hbox>
+			
+		</hbox>
+	</page>
+</widgets>
+~~
+p=page('test:p1')
+p.size(800,600)
+p.open()
+
+
+
+v=p.get('v')
+~~
+a=_arr()
+a.add('-vo')
+a.add('direct3d')
+a.add('C:/WORK/a.mp4')
+~~
+v.start(a)
+a.add('E:\기타\파묘 Exhuma.2024.1080p.WEB-DL.H264.AAC.mkv')
+
